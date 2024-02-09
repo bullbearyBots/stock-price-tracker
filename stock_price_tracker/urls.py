642d9b0920a1413354f2b31c5a50757aa2c76b8c
bullbearyBots <bullbeary.bots@gmail.com>
2024-02-09 18:53:01 +0100

    Added code for stock price visualization

--- stock_price_tracker/urls.py
+++ stock_price_tracker/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+import stock_visualizer.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", stock_visualizer.views.home),
+    path('get_stock_data/', stock_visualizer.views.get_stock_data),
 ]
